1a720b75bb52b7b5b025e0424335b485bbd41e78
Emad Gerges <emadgerges@Emads-MBP.lan>
2023-07-15 17:42:33 -0500

    update 3 0715

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
+import './app.css'
 
 function App() {
-  
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null)
-  console.log(puppies.id)
+  //console.log(puppies.id)
   console.log("puppyList: ", puppyList);
-  console.log(featPupId);
-
+  //console.log(featPupId);
+  //console.log(puppyList)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   return (
     <>
       <div className="App">{
         puppies.map((puppy) => {
         return (
-        <p key={puppy.id}>{puppy.name}</p>
-
-        )
-         })
-        
-        }
-        
+        <p className="click" onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        )} 
+         )
+        } <br></br>
+                <p>{ featPupId && (
+                  <div>
+                    <h2 className="puppy">
+                      {featuredPup.name}
+                    </h2>
+                    <ul className="puppyInfo">
+                      <li>Age: {featuredPup.age}</li>
+                      <li>Email: {featuredPup.email}</li>
+                    </ul>
+                  </div>)}
+                
+                </p>
+                 <p className="puppyId">{ featPupId }</p>
       </div>
       
       
