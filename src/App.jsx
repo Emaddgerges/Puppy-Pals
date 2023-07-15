84e1f062018ce5dc597646590492ced89eec3d80
Emad Gerges <emadgerges@Emads-MBP.lan>
2023-07-15 12:34:56 -0500

    update 0715

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
-import './App.css'
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
+  
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log(puppies.id)
+  console.log("puppyList: ", puppyList);
+  console.log(featPupId);
+
 
   return (
     <>
-      <div>
+      <div className="App">{
+        puppies.map((puppy) => {
+        return (
+        <p key={puppy.id}>{puppy.name}</p>
+
+        )
+         })
+        
+        }
         
       </div>
       
